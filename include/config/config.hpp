7b533e5037788a8030c2a97ad95f6d2dd38a6b26
ttldtor <ttldtor@gmail.com>
2025-10-05 01:22:29 +0300

    Add `Config` class with support for key-value storage and associated tests

--- include/config/config.hpp
+++ include/config/config.hpp
@@ -3,10 +3,39 @@
 
 #pragma once
 
-#include <concepts>
-
+#include <unordered_map>
+#include <string>
 
 namespace org::ttldtor::config {
 
+/**
+ * Simple configuration class.
+ */
+class Config {
+  std::unordered_map<std::string, std::string> data{};
 
-}
+ public:
+  /**
+   * Adds keys and values from the source.
+   *
+   * @tparam Source The source type.
+   * @param source The properties' source.
+   */
+  template <typename Source>
+  void addSource(Source&& source) {
+    for (auto&& [key, value] : source) {
+      data[key] = value;
+    }
+  }
+
+  /**
+   * Returns the collection of keys and values.
+   *
+   * @return The collection of keys and values.
+   */
+  [[nodiscard]] const auto& getData() const& {
+    return data;
+  }
+};
+
+}  // namespace org::ttldtor::config
--- tests/config.cpp
+++ tests/config.cpp
@@ -4,11 +4,63 @@
 #include <doctest/doctest.h>
 
 #include <config/config.hpp>
+#include <map>
+#include <string>
+#include <utility>
 
 using namespace org::ttldtor::config;
 
 TEST_CASE("Dummy test case") {
   SUBCASE("Dummy test") {
     CHECK(true);
+  }
+}
+
+template <typename M1, typename M2>
+bool equals(M1&& m1, M2&& m2) {
+  for (auto& [key, value] : m1) {
+    if (m2.find(key) == m2.end()) {
+      return false;
+    }
+
+    if (m2.at(key) != value) {
+      return false;
+    }
+  }
+
+  for (auto& [key, value] : m2) {
+    if (m1.find(key) == m1.end()) {
+      return false;
+    }
+
+    if (m1.at(key) != value) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+TEST_CASE("Config") {
+  SUBCASE("Adding keys and values.") {
+    std::map<std::string, std::string> properties{{"Key1", "Value1"}, {"Key2", "Value2"}};
+    auto config = Config();
+
+    config.addSource(properties);
+
+    CHECK(equals(properties, config.getData()));
+  }
+
+  SUBCASE("Subsequent additions of values with the same keys overwrite the old values.") {
+    std::map<std::string, std::string> properties{{"Key1", "Value1"}, {"Key2", "Value2"}};
+    std::map<std::string, std::string> properties2{{"Key2", "ValueX"}, {"Key3", "Value3"}};
+    auto config = Config();
+
+    config.addSource(properties);
+    config.addSource(properties2);
+
+    CHECK(
+      equals(std::unordered_map<std::string, std::string>{{"Key1", "Value1"}, {"Key2", "ValueX"}, {"Key3", "Value3"}},
+             config.getData()));
   }
 }
